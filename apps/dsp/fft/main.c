7f4ccb5a3429e968a25e73749e70c81813285ec7
Neal Jackson <nealjack@umich.edu>
2019-08-15 14:54:38 -0700

    Update fft app to ship data over uart
    push out real and complex fft bins

--- apps/dsp/fft/main.c
+++ apps/dsp/fft/main.c
@@ -4,6 +4,7 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include "nrf.h"
 #include "nrf_delay.h"
 #include "nrf_gpio.h"
@@ -17,7 +18,7 @@
 #include "arm_math.h"
 #include "arm_const_structs.h"
 
-#define TEST_LENGTH_SAMPLES 2048
+#define TEST_LENGTH_SAMPLES 8192
 
 NRF_SERIAL_DRV_UART_CONFIG_DEF(m_uart0_drv_config,
                       NRF_GPIO_PIN_MAP(0,6), NRF_GPIO_PIN_MAP(0,8),
@@ -40,13 +41,15 @@ NRF_SERIAL_BUFFERS_DEF(serial_buffs, SERIAL_BUFF_TX_SIZE, SERIAL_BUFF_RX_SIZE);
 NRF_SERIAL_CONFIG_DEF(serial_config, NRF_SERIAL_MODE_DMA,
                       &serial_queues, &serial_buffs, NULL, NULL);
 
-
 NRF_SERIAL_UART_DEF(serial_uart, 0);
 
 extern float testInput_f32_10khz[TEST_LENGTH_SAMPLES];
 static float input[TEST_LENGTH_SAMPLES] = {0};
 static float testOutput[TEST_LENGTH_SAMPLES/2];
-uint32_t fftSize = 1024;
+uint32_t fftSizes[6] = {128, 256, 512, 1024, 2048, 4096};
+const arm_cfft_instance_f32* instances[6] = {&arm_cfft_sR_f32_len128,
+    &arm_cfft_sR_f32_len256, &arm_cfft_sR_f32_len512, &arm_cfft_sR_f32_len1024,
+    &arm_cfft_sR_f32_len2048, &arm_cfft_sR_f32_len4096};
 uint32_t ifftFlag = 0;
 uint32_t doBitReverse = 1;
 uint32_t refIndex = 213, testIndex = 0;
@@ -75,52 +78,95 @@ int main(void) {
     err_code = nrf_serial_init(&serial_uart, &m_uart0_drv_config, &serial_config);
     printf("err_code = %x\n", err_code);
 
-    nrf_delay_ms(2000);
+    for (size_t i = 0; i < 6; i++) {
+        printf("\n\n\nSize: %u\n", fftSizes[i]);
+        printf("\nOriginal:\n");
+        for(size_t j=0; j < 2; j++) {
+            printf("%f ", testInput_f32_10khz[j]);
+        }
+        memcpy(input, testInput_f32_10khz, sizeof(float)*TEST_LENGTH_SAMPLES);
+        nrf_gpio_pin_toggle(LED1);
+        arm_cfft_f32(instances[i], input, 0, doBitReverse);
+        nrf_gpio_pin_toggle(LED1);
+        nrf_delay_ms(1);
 
-    printf("Original:\n");
-    for(size_t i=0; i < 10; i++) {
-        printf("%f ", testInput_f32_10khz[i]);
+        printf("\nraw fft bins:\n");
+        for(size_t j=0; j < 5; j++) {
+            printf("%f ", input[j]);
+        }
+        printf("\n");
+        for(size_t j=fftSizes[i]-1; j >= fftSizes[i]-1-5; j--) {
+            printf("%f ", input[j]);
+        }
+
+        /* Process the data through the Complex Magnitude Module for
+           calculating the magnitude at each bin */
+        nrf_gpio_pin_toggle(LED1);
+        arm_cmplx_mag_f32(input, testOutput, fftSizes[i]);
+        nrf_gpio_pin_toggle(LED1);
+        nrf_delay_ms(1);
+        printf("\nmag fft bins:\n");
+        for(size_t j=0; j < 5; j++) {
+            printf("%f ", testOutput[j]);
+        }
+        printf("\n");
+        for(size_t j=fftSizes[i]-1; j >= fftSizes[i]-1-4; j--) {
+            printf("%f ", testOutput[j]);
+        }
+
+
+        //nrf_gpio_pin_toggle(LED1);
+        //arm_cfft_f32(instances[i], input, 1, doBitReverse);
+        //nrf_gpio_pin_toggle(LED1);
+        //nrf_delay_ms(5);
+        //printf("\ninverted:\n");
+        //for(size_t j=0; j < 10; j++) {
+        //    printf("%f ", input[j]);
+        //}
+
+        //float error[TEST_LENGTH_SAMPLES] = {0};
+        //float total = 0;
+        //for(size_t j=0; j < TEST_LENGTH_SAMPLES; j++) {
+        //    float diff = input[j] - testInput_f32_10khz[j];
+        //    error[j] = diff*diff;
+        //    total += error[j];
+        //}
+        //printf("\ninverted error = %f\n", total);
+
+        /* Calculates maxValue and returns corresponding BIN value */
+        nrf_gpio_pin_toggle(LED1);
+        arm_max_f32(testOutput, fftSizes[i], &maxValue, &testIndex);
+        nrf_gpio_pin_toggle(LED1);
+        nrf_delay_ms(1);
+        printf("\nreal index: 0x%lx\n", refIndex);
+        printf("index: 0x%lx\n", testIndex);
+
+        // Send over UART
+        uint32_t len = sizeof(float) * fftSizes[i];
+        uint8_t *buf = malloc(len+6);
+        //[sizeof(float)*TEST_LENGTH_SAMPLES/2] = {0};
+        buf[0] = 0xAA;
+        buf[1] = 0xBB;
+        memcpy(buf+2, &len, sizeof(float));
+        memcpy(buf+6, testOutput, len);
+        nrf_serial_write(&serial_uart, buf, len+6, NULL, NRF_SERIAL_MAX_TIMEOUT);
+        free(buf);
+
+        // Send over UART
+        len = sizeof(float) * 2*fftSizes[i];
+        buf = malloc(len+6);
+        //[sizeof(float)*TEST_LENGTH_SAMPLES/2] = {0};
+        buf[0] = 0xAA;
+        buf[1] = 0xCC;
+        memcpy(buf+2, &len, sizeof(float));
+        memcpy(buf+6, input, len);
+        nrf_serial_write(&serial_uart, buf, len+6, NULL, NRF_SERIAL_MAX_TIMEOUT);
+        free(buf);
+
+        //nrf_delay_ms(500);
     }
-
-    while (1) {
-    memcpy(input, testInput_f32_10khz, sizeof(float)*TEST_LENGTH_SAMPLES);
-    nrf_gpio_pin_toggle(LED1);
-    arm_cfft_f32(&arm_cfft_sR_f32_len1024, input, ifftFlag, doBitReverse);
-    nrf_gpio_pin_toggle(LED1);
-    nrf_delay_ms(5);
-
-    /* Process the data through the Complex Magnitude Module for
-       calculating the magnitude at each bin */
-    nrf_gpio_pin_toggle(LED1);
-    arm_cmplx_mag_f32(input, testOutput, fftSize);
-    nrf_gpio_pin_toggle(LED1);
-    nrf_delay_ms(5);
-
-    printf("\nAfter:\n");
-    for(size_t i=0; i < 10; i++) {
-        printf("%f ", testOutput[i]);
-    }
-
-
-    /* Calculates maxValue and returns corresponding BIN value */
-    nrf_gpio_pin_toggle(LED1);
-    arm_max_f32(testOutput, fftSize, &maxValue, &testIndex);
-    nrf_gpio_pin_toggle(LED1);
-    nrf_delay_ms(5);
-    printf("\nreal index: 0x%lx\n", refIndex);
-    printf("index: 0x%lx\n", testIndex);
-
-    // Send over UART
-    uint32_t len = sizeof(float) * TEST_LENGTH_SAMPLES/2;
-    uint8_t buf[sizeof(float)*TEST_LENGTH_SAMPLES/2] = {0};
-    buf[0] = 0xAA;
-    buf[1] = 0xBB;
-    memcpy(buf+2, &len, sizeof(float));
-    memcpy(buf+6, testOutput, len);
-    err_code = nrf_serial_write(&serial_uart, buf, len+6, NULL, NRF_SERIAL_MAX_TIMEOUT);
-    printf("err_code = %x\n", err_code);
-
     // Enter main loop.
+    while(1){
         nrf_delay_ms(2000);
     }
 }
