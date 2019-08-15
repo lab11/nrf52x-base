0eadb04e8ba9047a2a483ff2117d79702ac3aa9f
Neal Jackson <nealjack@umich.edu>
2019-08-15 16:50:42 -0700

    fix build warnings for ble peer manager

--- lib/simple_ble/simple_ble.c
+++ lib/simple_ble/simple_ble.c
@@ -210,96 +210,96 @@ static void on_adv_evt(ble_adv_evt_t ble_adv_evt) {
     // do nothing
 }
 
-static void on_pm_evt(pm_evt_t const* p_evt) {
-    ret_code_t err_code;
-
-    switch (p_evt->evt_id)
-    {
-        case PM_EVT_BONDED_PEER_CONNECTED:
-        {
-            NRF_LOG_INFO("Connected to a previously bonded device.");
-        } break;
-
-        case PM_EVT_CONN_SEC_SUCCEEDED:
-        {
-            NRF_LOG_INFO("Connection secured: role: %d, conn_handle: 0x%x, procedure: %d.",
-                         ble_conn_state_role(p_evt->conn_handle),
-                         p_evt->conn_handle,
-                         p_evt->params.conn_sec_succeeded.procedure);
-        } break;
-
-        case PM_EVT_CONN_SEC_FAILED:
-        {
-            /* Often, when securing fails, it shouldn't be restarted, for security reasons.
-             * Other times, it can be restarted directly.
-             * Sometimes it can be restarted, but only after changing some Security Parameters.
-             * Sometimes, it cannot be restarted until the link is disconnected and reconnected.
-             * Sometimes it is impossible, to secure the link, or the peer device does not support it.
-             * How to handle this error is highly application dependent. */
-        } break;
-
-        case PM_EVT_CONN_SEC_CONFIG_REQ:
-        {
-            // Reject pairing request from an already bonded peer.
-            pm_conn_sec_config_t conn_sec_config = {.allow_repairing = false};
-            pm_conn_sec_config_reply(p_evt->conn_handle, &conn_sec_config);
-        } break;
-
-        case PM_EVT_STORAGE_FULL:
-        {
-            // Run garbage collection on the flash.
-            err_code = fds_gc();
-            if (err_code == FDS_ERR_NO_SPACE_IN_QUEUES)
-            {
-                // Retry.
-            }
-            else
-            {
-                APP_ERROR_CHECK(err_code);
-            }
-        } break;
-
-        case PM_EVT_PEERS_DELETE_SUCCEEDED:
-        {
-            advertising_start();
-        } break;
-
-        case PM_EVT_PEER_DATA_UPDATE_FAILED:
-        {
-            // Assert.
-            APP_ERROR_CHECK(p_evt->params.peer_data_update_failed.error);
-        } break;
-
-        case PM_EVT_PEER_DELETE_FAILED:
-        {
-            // Assert.
-            APP_ERROR_CHECK(p_evt->params.peer_delete_failed.error);
-        } break;
-
-        case PM_EVT_PEERS_DELETE_FAILED:
-        {
-            // Assert.
-            APP_ERROR_CHECK(p_evt->params.peers_delete_failed_evt.error);
-        } break;
-
-        case PM_EVT_ERROR_UNEXPECTED:
-        {
-            // Assert.
-            APP_ERROR_CHECK(p_evt->params.error_unexpected.error);
-        } break;
-
-        case PM_EVT_CONN_SEC_START:
-        case PM_EVT_PEER_DATA_UPDATE_SUCCEEDED:
-        case PM_EVT_PEER_DELETE_SUCCEEDED:
-        case PM_EVT_LOCAL_DB_CACHE_APPLIED:
-        case PM_EVT_LOCAL_DB_CACHE_APPLY_FAILED:
-            // This can happen when the local DB has changed.
-        case PM_EVT_SERVICE_CHANGED_IND_SENT:
-        case PM_EVT_SERVICE_CHANGED_IND_CONFIRMED:
-        default:
-            break;
-    }
-}
+//static void on_pm_evt(pm_evt_t const* p_evt) {
+//    ret_code_t err_code;
+//
+//    switch (p_evt->evt_id)
+//    {
+//        case PM_EVT_BONDED_PEER_CONNECTED:
+//        {
+//            NRF_LOG_INFO("Connected to a previously bonded device.");
+//        } break;
+//
+//        case PM_EVT_CONN_SEC_SUCCEEDED:
+//        {
+//            NRF_LOG_INFO("Connection secured: role: %d, conn_handle: 0x%x, procedure: %d.",
+//                         ble_conn_state_role(p_evt->conn_handle),
+//                         p_evt->conn_handle,
+//                         p_evt->params.conn_sec_succeeded.procedure);
+//        } break;
+//
+//        case PM_EVT_CONN_SEC_FAILED:
+//        {
+//            /* Often, when securing fails, it shouldn't be restarted, for security reasons.
+//             * Other times, it can be restarted directly.
+//             * Sometimes it can be restarted, but only after changing some Security Parameters.
+//             * Sometimes, it cannot be restarted until the link is disconnected and reconnected.
+//             * Sometimes it is impossible, to secure the link, or the peer device does not support it.
+//             * How to handle this error is highly application dependent. */
+//        } break;
+//
+//        case PM_EVT_CONN_SEC_CONFIG_REQ:
+//        {
+//            // Reject pairing request from an already bonded peer.
+//            pm_conn_sec_config_t conn_sec_config = {.allow_repairing = false};
+//            pm_conn_sec_config_reply(p_evt->conn_handle, &conn_sec_config);
+//        } break;
+//
+//        case PM_EVT_STORAGE_FULL:
+//        {
+//            // Run garbage collection on the flash.
+//            err_code = fds_gc();
+//            if (err_code == FDS_ERR_NO_SPACE_IN_QUEUES)
+//            {
+//                // Retry.
+//            }
+//            else
+//            {
+//                APP_ERROR_CHECK(err_code);
+//            }
+//        } break;
+//
+//        case PM_EVT_PEERS_DELETE_SUCCEEDED:
+//        {
+//            advertising_start();
+//        } break;
+//
+//        case PM_EVT_PEER_DATA_UPDATE_FAILED:
+//        {
+//            // Assert.
+//            APP_ERROR_CHECK(p_evt->params.peer_data_update_failed.error);
+//        } break;
+//
+//        case PM_EVT_PEER_DELETE_FAILED:
+//        {
+//            // Assert.
+//            APP_ERROR_CHECK(p_evt->params.peer_delete_failed.error);
+//        } break;
+//
+//        case PM_EVT_PEERS_DELETE_FAILED:
+//        {
+//            // Assert.
+//            APP_ERROR_CHECK(p_evt->params.peers_delete_failed_evt.error);
+//        } break;
+//
+//        case PM_EVT_ERROR_UNEXPECTED:
+//        {
+//            // Assert.
+//            APP_ERROR_CHECK(p_evt->params.error_unexpected.error);
+//        } break;
+//
+//        case PM_EVT_CONN_SEC_START:
+//        case PM_EVT_PEER_DATA_UPDATE_SUCCEEDED:
+//        case PM_EVT_PEER_DELETE_SUCCEEDED:
+//        case PM_EVT_LOCAL_DB_CACHE_APPLIED:
+//        case PM_EVT_LOCAL_DB_CACHE_APPLY_FAILED:
+//            // This can happen when the local DB has changed.
+//        case PM_EVT_SERVICE_CHANGED_IND_SENT:
+//        case PM_EVT_SERVICE_CHANGED_IND_CONFIRMED:
+//        default:
+//            break;
+//    }
+//}
 
 static void on_ble_evt(ble_evt_t const * p_ble_evt, void * p_context) {
     uint32_t err_code;
@@ -731,36 +731,37 @@ void __attribute__((weak)) services_init(void) {
 #define SEC_PARAM_OOB                   0                                       /**< Out Of Band data not available. */
 #define SEC_PARAM_MIN_KEY_SIZE          7                                       /**< Minimum encryption key size. */
 #define SEC_PARAM_MAX_KEY_SIZE          16                                      /**< Maximum encryption key size. */
-static void peer_manager_init(void)
-{
-    ble_gap_sec_params_t sec_param;
-    ret_code_t           err_code;
 
-    err_code = pm_init();
-    APP_ERROR_CHECK(err_code);
-
-    memset(&sec_param, 0, sizeof(ble_gap_sec_params_t));
-
-    // Security parameters to be used for all security procedures.
-    sec_param.bond           = SEC_PARAM_BOND;
-    sec_param.mitm           = SEC_PARAM_MITM;
-    sec_param.lesc           = SEC_PARAM_LESC;
-    sec_param.keypress       = SEC_PARAM_KEYPRESS;
-    sec_param.io_caps        = SEC_PARAM_IO_CAPABILITIES;
-    sec_param.oob            = SEC_PARAM_OOB;
-    sec_param.min_key_size   = SEC_PARAM_MIN_KEY_SIZE;
-    sec_param.max_key_size   = SEC_PARAM_MAX_KEY_SIZE;
-    sec_param.kdist_own.enc  = 1;
-    sec_param.kdist_own.id   = 1;
-    sec_param.kdist_peer.enc = 1;
-    sec_param.kdist_peer.id  = 1;
-
-    err_code = pm_sec_params_set(&sec_param);
-    APP_ERROR_CHECK(err_code);
-
-    err_code = pm_register(on_pm_evt);
-    APP_ERROR_CHECK(err_code);
-}
+//static void peer_manager_init(void)
+//{
+//    ble_gap_sec_params_t sec_param;
+//    ret_code_t           err_code;
+//
+//    err_code = pm_init();
+//    APP_ERROR_CHECK(err_code);
+//
+//    memset(&sec_param, 0, sizeof(ble_gap_sec_params_t));
+//
+//    // Security parameters to be used for all security procedures.
+//    sec_param.bond           = SEC_PARAM_BOND;
+//    sec_param.mitm           = SEC_PARAM_MITM;
+//    sec_param.lesc           = SEC_PARAM_LESC;
+//    sec_param.keypress       = SEC_PARAM_KEYPRESS;
+//    sec_param.io_caps        = SEC_PARAM_IO_CAPABILITIES;
+//    sec_param.oob            = SEC_PARAM_OOB;
+//    sec_param.min_key_size   = SEC_PARAM_MIN_KEY_SIZE;
+//    sec_param.max_key_size   = SEC_PARAM_MAX_KEY_SIZE;
+//    sec_param.kdist_own.enc  = 1;
+//    sec_param.kdist_own.id   = 1;
+//    sec_param.kdist_peer.enc = 1;
+//    sec_param.kdist_peer.id  = 1;
+//
+//    err_code = pm_sec_params_set(&sec_param);
+//    APP_ERROR_CHECK(err_code);
+//
+//    err_code = pm_register(on_pm_evt);
+//    APP_ERROR_CHECK(err_code);
+//}
 
 #ifdef ENABLE_DFU
 void __attribute__((weak)) dfu_init (void) {
