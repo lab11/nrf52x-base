12f43b1ee58ad89b89f69a8da8b48b41c3cd0cd7
Neal Jackson <nealjack@umich.edu>
2020-04-08 21:38:16 -0700

    fix compile for simple zigbee

--- lib/simple_zigbee/simple_zigbee.c
+++ lib/simple_zigbee/simple_zigbee.c
@@ -12,11 +12,11 @@ void fix_errata_78_in_nrf_802154(void)
      * Issuing task SHUTDOWN fixes this condition. This have to be removed when
      * proper fix will be pushed to nRF-IEEE-802.15.4-radio-driver.
      */
-    if ((otPlatRadioGetState(m_ot_instance) == OT_RADIO_STATE_SLEEP) ||
-        (otPlatRadioGetState(m_ot_instance) == OT_RADIO_STATE_DISABLED))
-    {
-        nrf_timer_task_trigger(NRF_TIMER1, NRF_TIMER_TASK_SHUTDOWN);
-    }
+    //if ((otPlatRadioGetState(m_ot_instance) == OT_RADIO_STATE_SLEEP) ||
+    //    (otPlatRadioGetState(m_ot_instance) == OT_RADIO_STATE_DISABLED))
+    //{
+        //nrf_timer_task_trigger(NRF_TIMER1, NRF_TIMER_TASK_SHUTDOWN);
+    //}
     #if (__FPU_USED == 1)
     __set_FPSCR(__get_FPSCR() & ~(0x0000009F));
     (void) __get_FPSCR();
@@ -34,7 +34,6 @@ void __attribute__((weak)) zigbee_sleep(void)
 //        ASSERT(err_code == NRF_SUCCESS);
 //#else
         //__WFE();
-    zb_sleep_now()
+    zb_sleep_now();
 //#endif
-    }
 }
